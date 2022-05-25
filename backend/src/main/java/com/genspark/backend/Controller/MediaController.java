571c5775f6ee0c8fbc3b2f9e264ecdaea8d7660b
Michael Maganini <michaelmags33@gmail.com>
2022-05-25 14:22:06 -0500

    SearchPage: improved UI and refactored code a bit

--- backend/src/main/java/com/genspark/backend/Controller/MediaController.java
+++ backend/src/main/java/com/genspark/backend/Controller/MediaController.java
@@ -9,21 +9,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.genspark.backend.Security.SecurityUtil.execAuthHeaderActions;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 /**
- *  REST controller mapping for searching for shows or movies
+ * REST controller mapping for searching for shows or movies
  */
 @RestController
-@RequestMapping("/api/media")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MediaController {
     private final AppUserService appUserService;
 
     /**
      * Conducts search from user's inputted search parameters
+     *
      * @param searchDetails object containing details of a user's search
      * @return 200 OK response entity with List<Media> search results body if successful, unprocessableEntity body otherwise
      * @throws IOException
@@ -38,19 +44,36 @@ public class MediaController {
                 .body(StreamingAvailableAPI.getMediaAvailability(searchDetails.getCountry(), searchDetails.getService(),
                         searchDetails.getType(), "en", "", searchDetails.getKeyword()));
     }
+
+    /**
+     * Gets a new JWT access token from a refresh token
+     *
+     * @param request  incoming REST HTTP request
+     * @param response outgoing REST HTTP response
+     * @throws IOException
+     */
+    @GetMapping("/tokens/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            execAuthHeaderActions(authorizationHeader, appUserService, request, response);
+        } else {
+            throw new RuntimeException("Refresh token is missing");
+        }
+    }
 }
 
 /**
- *  object containing details of a user's search from frontend
+ * object containing details of a user's search from frontend
  */
 @Data
 @AllArgsConstructor
 class SearchDetails {
-    //country code: us, ca, gb, de, fr, it, au, mx, br, es, in, id, ru, jp, th, kr
+    //country code, possible values: us, ca, gb, de, fr, it, au, mx, br, es, in, id, ru, jp, th, kr
     private String country;
-    //streaming service: netflix, prime, disney, apple, mubi, curiosity, zee5
+    //streaming service, possible values: netflix, prime, disney, apple, mubi, curiosity, zee5
     private String service;
-    //type of media: movie, series
+    //type of media, possible values: movie, series
     private String type;
     private String keyword;
 }
--- frontend/src/components/SearchPage.jsx
+++ frontend/src/components/SearchPage.jsx
@@ -1,6 +1,61 @@
 import MovieCard from "./MovieCard";
 import React, { useState } from 'react';
-import { InputLabel, FormControl, Select, MenuItem, TextField, Button } from '@mui/material';
+import { InputLabel, FormControl, Select, MenuItem, TextField, Button, Box, Stack, Paper } from '@mui/material';
+
+const formsContainerStyle = {
+    padding: 2,
+    margin: 3,
+    resize: "horizontal",
+    width: 1000,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+
+const searchFormsStyle = {
+    display: 'flex',
+    alignItems: 'center'
+}
+
+const singleFormStyle = {
+    width: 300,
+    margin: 1
+}
+
+const countryFormItems = [
+    { value: "us", label: "United States" },
+    { value: "ca", label: "Canada" },
+    { value: "gb", label: "United Kingdom" },
+    { value: "de", label: "Germany" },
+    { value: "fr", label: "France" },
+    { value: "it", label: "Italy" },
+    { value: "au", label: "Australia" },
+    { value: "mx", label: "Mexico" },
+    { value: "br", label: "Brazil" },
+    { value: "es", label: "Spain" },
+    { value: "in", label: "India" },
+    { value: "id", label: "Indonesia" },
+    { value: "ru", label: "Russia" },
+    { value: "jp", label: "Japan" },
+    { value: "th", label: "Thailand" },
+    { value: "kr", label: "South Korea" },
+]
+
+const serviceFormItems = [
+    { value: "apple", label: "Apple TV" },
+    { value: "curiosity", label: "Curiosity Stream" },
+    { value: "disney", label: "Disney Plus" },
+    { value: "mubi", label: "MUBI" },
+    { value: "netflix", label: "Netflix" },
+    { value: "prime", label: "Prime Video" },
+    { value: "zee5", label: "ZEE5" }
+]
+
+const typeFormItems = [
+    { value: "movie", label: "Movie" },
+    { value: "series", label: "Series" },
+]
 
 
 const Search = () => {
@@ -27,96 +82,80 @@ const Search = () => {
         setKeyword(event.target.value);
     };
 
+
+
     const handleSearch = () => {
         var json = JSON.stringify({
-			country: country, service: service, type: type, keyword: keyword
-		})
+            country: country, service: service, type: type, keyword: keyword
+        })
         console.log(json)
-        const fetchFrom = 'http://localhost:8080/api/media/search';
-		const payload = {
-			method: 'POST',
-			headers: {
-				"Content-Type": "application/json",
-			},
-            body: json
-		}
-        fetch(fetchFrom, payload)
-		.then(response => {
-			if (!response.ok) throw new Error(response.status);
-			else return response.json();
-		})
-		.then((searchResponse) => {
-			setSearchResults(searchResponse);
-            console.log(searchResponse)
-			window.alert("Search success!")
-		})
-		.catch((error) => {
-			window.alert("Search failed!")
-			console.log("Search failed: " + error)
-		})
+        // const fetchFrom = 'http://localhost:8080/api/search';
+        // const payload = {
+        //     method: 'POST',
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        //     body: json
+        // }
+        // fetch(fetchFrom, payload)
+        //     .then(response => {
+        //         if (!response.ok) throw new Error(response.status);
+        //         else return response.json();
+        //     })
+        //     .then((searchResponse) => {
+        //         setSearchResults(searchResponse);
+        //         console.log(searchResponse)
+        //         window.alert("Search success!")
+        //     })
+        //     .catch((error) => {
+        //         window.alert("Search failed!")
+        //         console.log("Search failed: " + error)
+        //     })
     };
 
+    const forms = [
+        { id: "type-form", label: "Type", value: type, handleMethod: handleChangeType, formItems: typeFormItems },
+        { id: "service-form", label: "Service", value: service, handleMethod: handleChangeService, formItems: serviceFormItems },
+        { id: "country-form", label: "Country", value: country, handleMethod: handleChangeCountry, formItems: countryFormItems }
+    ]
+
     return (
-        <div>
-            <FormControl >
-                <InputLabel id="type-form">Type</InputLabel>
-                <Select
-                    labelId="type-form"
-                    value={type}
-                    label="Type"
-                    onChange={handleChangeType}
-                >
-                    <MenuItem value={"movie"}>Movie</MenuItem>
-                    <MenuItem value={"series"}>Series</MenuItem>
-                </Select>
-            </FormControl>
-            <FormControl >
-                <InputLabel id="service-form">Service</InputLabel>
-                <Select
-                    labelId="service-form"
-                    value={service}
-                    label="Service"
-                    onChange={handleChangeService}
-                >
-                    <MenuItem value={"apple"}>Apple TV</MenuItem>
-                    <MenuItem value={"curiosity"}>Curiosity Stream</MenuItem>
-                    <MenuItem value={"disney"}>Disney Plus</MenuItem>
-                    <MenuItem value={"mubi"}>MUBI</MenuItem>
-                    <MenuItem value={"netflix"}>Netflix</MenuItem>
-                    <MenuItem value={"prime"}>Prime Video</MenuItem>
-                    <MenuItem value={"zee5"}>ZEE5</MenuItem>
-                </Select>
-            </FormControl>
-            <FormControl >
-                <InputLabel id="country-form">Country</InputLabel>
-                <Select
-                    labelId="country-form"
-                    value={country}
-                    label="Country"
-                    onChange={handleChangeCountry}
-                >
-                    <MenuItem value={"us"}>United States</MenuItem>
-                    <MenuItem value={"ca"}>Canada</MenuItem>
-                    <MenuItem value={"gb"}>United Kingdom</MenuItem>
-                    <MenuItem value={"de"}>Germany</MenuItem>
-                    <MenuItem value={"fr"}>France</MenuItem>
-                    <MenuItem value={"it"}>Italy</MenuItem>
-                    <MenuItem value={"au"}>Australia</MenuItem>
-                    <MenuItem value={"mx"}>Mexico</MenuItem>
-                    <MenuItem value={"br"}>Brazil</MenuItem>
-                    <MenuItem value={"es"}>Spain</MenuItem>
-                    <MenuItem value={"in"}>India</MenuItem>
-                    <MenuItem value={"id"}>Indonesia</MenuItem>
-                    <MenuItem value={"ru"}>Russia</MenuItem>
-                    <MenuItem value={"jp"}>Japan</MenuItem>
-                    <MenuItem value={"th"}>Thailand</MenuItem>
-                    <MenuItem value={"kr"}>South Korea</MenuItem>
-                </Select>
-            </FormControl>
-            <TextField label="Keyword" variant="outlined" onChange={handleChangeKeyword}/>
-            <Button variant="outlined" onClick={handleSearch}>Search</Button>
-        </div>
+        <Box sx={{display: 'flex', justifyContent: 'center'}}>
+            <Paper elevation={3} sx={formsContainerStyle}>
+                <Box sx={searchFormsStyle}>
+                    {forms.map((form) => getForm(form))}
+                </Box>
+                <Box sx={searchFormsStyle}>
+                    <TextField required sx={singleFormStyle} label="Keyword" variant="outlined" onChange={handleChangeKeyword} />
+                    <Button sx={{ width: 100, height: 50 }} variant="outlined" onClick={handleSearch}>Search</Button>
+                </Box>
+            </Paper>
+        </Box>
     );
+}
+
+function getForm(form) {
+    const { id, label, value, handleMethod, formItems } = form
+    return (
+        <FormControl required sx={singleFormStyle}>
+            <InputLabel id={id}>{label}</InputLabel>
+            <Select
+                labelId={id}
+                value={value}
+                label={label}
+                onChange={handleMethod}
+            >
+                {formItems.map((formItem) => getFormItems(formItem))}
+            </Select>
+        </FormControl>
+    )
+}
+
+function getFormItems(formItem) {
+    const { label, value } = formItem
+    return (
+        <MenuItem value={value}>{label}</MenuItem>
+    )
 }
 
 export default Search;
