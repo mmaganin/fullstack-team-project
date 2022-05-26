3c8957d9980bb52f69bb5261067ae7f2a48b80bb
Michael Maganini <michaelmags33@gmail.com>
2022-05-26 14:47:45 -0500

    Finished commenting MediaCard component

--- frontend/src/components/MediaCard.jsx
+++ frontend/src/components/MediaCard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Card, Box, Divider, IconButton, Menu, MenuItem } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
 
+//Styles
 const cardStyle = {
     display: 'flex',
     justifyContent: 'space-between',
@@ -9,16 +10,13 @@ const cardStyle = {
     width: 1200,
     margin: 2,
 }
-
-const imgContainerStyle = { 
-    minWidth: 145, width: 145, marginTop: 'auto', marginBottom: 'auto' 
+const imgContainerStyle = {
+    minWidth: 145, width: 145, marginTop: 'auto', marginBottom: 'auto'
 }
-
 const imgStyle = {
     width: '100%',
     height: 'auto',
 }
-
 const cardSectionStyle = {
     display: 'flex',
     flexDirection: 'column',
@@ -28,46 +26,59 @@ const cardSectionStyle = {
     marginLeft: 2,
     marginRight: 2,
 }
-
 const menuButtonStyle = {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
 }
-
-const sectionTitleStyle = { 
-    typography: 'subtitle2', 
-    fontStyle: 'italic', 
-    fontSize: 18 
+const sectionTitleStyle = {
+    typography: 'subtitle2',
+    fontStyle: 'italic',
+    fontSize: 18
 }
-
-const sectionBodyStyle = { 
-    typography: 'body2', width: '100%' 
+const sectionBodyStyle = {
+    typography: 'body2',
+    width: '100%'
 }
-
+/**
+ * Component that displays a single search result for a movie or TV series which gets mapped onto the homepage after a successful search
+ * @param {{id: Number, imdbID: String, tmdbID: String, imdbRating: String, imdbVoteCount: Number, tmdbRating: Number, 
+ * originalTitle: String, countries: Array<String>, year: Number, runtime: Number, cast: Array<String>, significants: Array<String>, 
+ * title: String, overview: String, tagline: Number, video: Number, age: Number, originalLanguage: String, posterUrlOriginal: String, 
+ * firstAirYear: String, lastAirYear: String, episodeRuntimes: Array<String>, seasons: Number, episodes: Number, status: String}} props Media object from backend, DB entity
+ * 
+ * @returns Card component for single TV series or movie search result on homepage
+ */
 const MediaCard = (props) => {
+    //extracts required variables from props
     const { posterUrlOriginal, title, originalTitle, year, overview, cast, significants,
         episodes, firstAirYear, lastAirYear, seasons, imdbRating, imdbVoteCount } = props
-
-    const MENU_HEIGHT = 48;
+    //configures specific props to be displayable on card
     var displayOverview = getStringOfMaxSize(overview, 225);
     var displayCast = getStringOfMaxSize(mapArrtoCsvString(cast), 100);
     var displaySignificants = getStringOfMaxSize(mapArrtoCsvString(significants), 50);
-
+    //height of single menu item for menu at right side of card (after clicking triple horizontal line icon)
+    const MENU_HEIGHT = 48;
+    //states for opening and closing menu
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
+    //functions that open and close menu
     const handleClickMenu = (event) => {
         setAnchorEl(event.currentTarget);
     };
     const handleCloseMenu = () => {
         setAnchorEl(null);
     };
-
+    //adds media to must watch list and closes menu
     function handleAddToList() {
         console.log(title + " added to MUST WATCH LIST")
         setAnchorEl(null);
     }
-
+    /**
+     * provides functionality for the menu that is displayed upon clicking triple 
+     * horizontal line icon at right side of MediaCard
+     * @returns Box container with IconButton (triple horizontal line) and Menu (menu option Add to List) child components
+     */
     function getMenuButton() {
         return (
             <Box sx={menuButtonStyle}>
@@ -80,7 +91,7 @@ const MediaCard = (props) => {
                     sx={{ margin: 1 }}
                 >
                     <MenuIcon />
-                </IconButton>
+                </IconButton> {/*button with 3 horizontal lines used for adding media to must watch list */}
                 <Menu
                     id="long-menu"
                     MenuListProps={{
@@ -98,31 +109,9 @@ const MediaCard = (props) => {
                 >
                     <MenuItem key="add-to-must-watch-list" onClick={handleAddToList}>
                         Add To List
-                    </MenuItem>
-                </Menu>
-            </Box>
-        )
-    }
-
-    function getCardSection(props) {
-        const { title, sectionInfo } = props
-        var checkData = sectionInfo.map((info) => isFieldPresent(info.value))
-        if (!checkData.includes(true)) return "";
-        return (
-            <>
-                <Box sx={cardSectionStyle}>
-                    <Box sx={sectionTitleStyle}>{title}</Box>
-                    <Box sx={sectionBodyStyle}>
-                        {sectionInfo.map((info) => {
-                            var displayLabel = info.label === "" ? ""
-                                : info.label + ": "
-                            return !isFieldPresent(info.value) ? ""
-                                : <p style={{ margin: 2 }}><strong>{displayLabel}</strong>{info.value}</p>
-                        })}
-                    </Box>
-                </Box>
-                <Divider orientation="vertical" flexItem />
-            </>
+                    </MenuItem>{/*single menu item for adding media to must watch list */}
+                </Menu>{/*menu of options that appears upon clicking IconButton */}
+            </Box>/*container for menu icon button on right side of search result media card */
         )
     }
 
@@ -130,7 +119,7 @@ const MediaCard = (props) => {
         <Card raised sx={cardStyle}>
             <Box sx={imgContainerStyle}>
                 <img src={posterUrlOriginal} alt="" style={imgStyle} />
-            </Box>
+            </Box>{/*Section of card with poster image of movie or TV series */}
             {getCardSection({
                 title: "General Info",
                 sectionInfo: [
@@ -151,7 +140,7 @@ const MediaCard = (props) => {
                         value: displayCast
                     },
                 ]
-            })}
+            })}{/*Section of card with general info about movie or TV series */}
             {getCardSection({
                 title: "Series Info",
                 sectionInfo: [
@@ -172,7 +161,7 @@ const MediaCard = (props) => {
                         value: lastAirYear
                     },
                 ]
-            })}
+            })}{/*Section of card with details only relevant for TV series */}
             {getCardSection({
                 title: "Overview",
                 sectionInfo: [
@@ -182,7 +171,7 @@ const MediaCard = (props) => {
                     },
 
                 ]
-            })}
+            })}{/*Section of card with overview summary of the movie or TV series */}
             {getCardSection({
                 title: "IMDB Stats",
                 sectionInfo: [
@@ -196,11 +185,42 @@ const MediaCard = (props) => {
                     },
                 ]
             })}
-            {getMenuButton()}
-        </Card>
+            {getMenuButton()} {/*Section of card with IMDB data for the movie or TV series */}
+        </Card> /*Card container for a displaying a search result, has shadow and rounded border */
     );
 }
-
+/**
+ * single section of card containing data for movie or TV show
+ * @param {{title: String, sectionInfo: Array<{label: String, value: String}>}} props contains the title for the section and the info to display within the section
+ * @returns single section of card containing data for movie or TV show (Box container) 
+ * and a Divider that divides each section with a vertical line
+ */
+function getCardSection(props) {
+    const { title, sectionInfo } = props
+    var checkData = sectionInfo.map((info) => isFieldPresent(info.value))
+    if (!checkData.includes(true)) return "";
+    return (
+        <>
+            <Box sx={cardSectionStyle}>
+                <Box sx={sectionTitleStyle}>{title}</Box>{/*text title of card section */}
+                <Box sx={sectionBodyStyle}>
+                    {sectionInfo.map((info) => {
+                        var displayLabel = info.label === "" ? ""
+                            : info.label + ": "
+                        return !isFieldPresent(info.value) ? ""
+                            : <p style={{ margin: 2 }}><strong>{displayLabel}</strong>{info.value}</p>
+                    })}
+                </Box>{/*text body of card section */}
+            </Box>{/*single section of card containing data for movie or TV show */}
+            <Divider orientation="vertical" flexItem />{/*vertical divider separating different sections of the card */}
+        </>
+    )
+}
+/**
+ * Turns an array of Strings into a single comma separated string
+ * @param {Array<String>} arr any array of strings
+ * @returns String containing all strings in array separated by a comma and a space
+ */
 function mapArrtoCsvString(arr) {
     var string = ""
     for (let i = 0; i < arr.length; i++) {
@@ -212,12 +232,21 @@ function mapArrtoCsvString(arr) {
 
     return string
 }
-
+/**
+ * Decreases the size of a string to fit inside a section of the MediaCard
+ * @param {String} string any string
+ * @param {Number} maxSize Maximum number of characters for the string
+ * @returns a String of decreased size if its number of characters exceeds maxSize
+ */
 function getStringOfMaxSize(string, maxSize) {
     return Array.from(string).length <= maxSize ? string
         : string.substring(0, maxSize) + "...";
 }
-
+/**
+ * Determines if a variable/field is present/empty or not
+ * @param {any} variable 
+ * @returns true if variable is not empty, null, "", or undefined, false otherwise
+ */
 function isFieldPresent(variable) {
     if (variable === undefined || variable === null || variable === "" || variable === []) return false;
     return true;
